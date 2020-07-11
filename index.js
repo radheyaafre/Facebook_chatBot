7d03fb3980c617e4d493d675d44dc66292b83492
radheya afre <radheya.afre@oracle.com>
2020-07-11 14:33:01 +0530

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-'user stricts'
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+//This is still work in progress
+/*
+Please report any bugs to nicomwaks@gmail.com
+i have added console.log on line 48 
+ */
+'use strict'
 
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-app.set('port',(process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-//alows to process data
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-//routes
-app.get('/', function(req, res) {
-    res.send('Hi I am chatbot');
-});
+// parse application/json
+app.use(bodyParser.json())
 
-//Fb
-app.get('/webhook/', function(req, res){
-   if(req.query['hub.varify_token'] === "blondibytes"){
-       res.send(req.query['hub.challange']);
-   } 
-   res.send("Wrong toen");
-});
+// index
+app.get('/', function (req, res) {
+	res.send('hello world i am a secret bot')
+})
 
-app.listen(app.get('port'), function(){
-    console.log('Running : port');
-});
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	} else {
+		res.send('Error, wrong token')
+	}
+})
+
+// spin spin sugar
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
