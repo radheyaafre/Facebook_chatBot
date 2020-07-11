08992ec42b903bb5101cb378204f63530b2a14df
radheya afre <radheya.afre@oracle.com>
2020-07-11 18:31:15 +0530

    index.js

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.post('/webhook/', function (req, res) {
 		if (event.message && event.message.text) {
 			let text = event.message.text
 			console.log(text);
+			console.log('Got the messgae: ',text);
 			if (text === 'Generic'){ 
 				console.log("welcome to chatbot")
 				//sendGenericMessage(sender)
